5e47e4b9c4887f71e6a243473433222a2cee17f3
Pavel Kalenik <kalenik.paul@gmail.com>
2019-04-07 20:54:58 +0300

    Был написан геймплей(механика) шашек. Добавлены модели, логика игры(правильное перемещение шашек, переход в дамки и возможность побить шашку).

---
+++ Assets/Scripts/CheckersBoard.cs
@@ -0,0 +1,368 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckersBoard : MonoBehaviour
+{
+    public Piece[,] pieces = new Piece[8,8];
+    public GameObject whitePiecePrefab;
+    public GameObject blackPiecePrefab;
+
+    private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
+    private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
+
+    // Для проверки цвета шашки
+    public bool isWhite;
+    private bool isWhiteTurn;
+    private bool hasKilled;
+
+    private Piece selectedPiece;
+    private List<Piece> forcedPieces;
+    private Vector2 mouseOver;
+    private Vector2 startDrag;
+    private Vector2 endDrag;
+
+    void Start()
+    {
+        // Чтобы первыми начинали белые
+        isWhite = true;
+        isWhiteTurn = true;
+        forcedPieces = new List<Piece>();
+        GenerateBoard();
+    }
+
+    void Update()
+    {
+        UpdateMouseOver();
+        
+        // if it is my turn
+        if((isWhite)?isWhiteTurn:!isWhiteTurn)
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+
+            if(selectedPiece != null)
+            {
+                UpdatePieceDrag(selectedPiece);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                SelectPiece(x, y);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                TryMove((int)startDrag.x, (int)startDrag.y, x, y);
+                //Debug.Log(startDrag.x + " " + startDrag.y + " " + x + " " + y);
+            }
+        }
+    }
+
+    private void UpdateMouseOver()
+    {
+        // if its my turn 
+        // camera check
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }  
+
+        RaycastHit hit;
+        
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            // изначально было неправильное смещение, по этому нужно отнять boardOffset, чтобы начиналось с 0
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
+
+    }
+
+    private void UpdatePieceDrag(Piece p)
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }  
+
+        RaycastHit hit;
+        
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        // Out of bounds
+        if (x < 0 || x >= 8|| y < 0 || y >= 8)
+        {
+            return;
+        }
+
+        Piece p = pieces[x, y];
+        if (p != null && p.isWhite == isWhite)
+        {
+            if(forcedPieces.Count == 0)
+            {
+                selectedPiece = p;
+                startDrag = mouseOver;
+                Debug.Log(selectedPiece.name + "Start: " + startDrag);
+            }
+            else
+            {
+                // Look for the piece our forced pieces List
+                // Мы не смогли найти шашку, которую мы на самом деле выбираем
+                if(forcedPieces.Find(fp => fp == p) == null)
+                    return;
+                
+                // Если смогли найти
+                selectedPiece = p;
+                startDrag = mouseOver;
+            }
+        }
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+        forcedPieces = ScanForPossibleMove();
+
+        // Multiplayer Support
+        startDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+
+        //MovePiece(selectedPiece, x2, y2);
+
+        // Out of bounds
+        if (x2 < 0 || x2 >= 8|| y2 < 0 || y2 >= 8)
+        {
+            //Если мы нажали вне границы то переносим шашку на стартувую позицию
+            if(selectedPiece != null)
+            {
+                MovePiece(selectedPiece, x1, y1);
+            }
+
+            startDrag = Vector2.zero;
+            selectedPiece = null;
+            return;
+        }
+
+        if(selectedPiece != null)
+        {
+            // If it has not moved (если шашку не перемещали)
+            if(endDrag == startDrag)
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
+
+            // Check if its a valid move
+            if(selectedPiece.ValidMove(pieces, x1, y1, x2, y2))
+            {
+                // Did we kill anything
+                // If this is a jump
+                if(Mathf.Abs(x2-x1) == 2)
+                {   
+                    // Для получения средней шашки между двумя
+                    Piece p = pieces[(x1+x2)/2, (y1+y2)/2];
+                    if(p != null)
+                    {
+                        // Удаляем среднюю шашку между двумя
+                        pieces[(x1+x2)/2, (y1+y2)/2] = null;
+                        Destroy(p.gameObject);
+                        hasKilled = true;
+                    }
+                }
+
+                // Were we suposed to kill anything?
+                if(forcedPieces.Count != 0 && !hasKilled)
+                {
+                    MovePiece(selectedPiece, x1, y1);
+                    startDrag = Vector2.zero;
+                    selectedPiece = null;
+                    return;
+                }
+
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+                MovePiece(selectedPiece, x2, y2);
+
+                EndTurn();
+            }
+            else
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+            }
+        }
+
+        // Check if we are out of bounds
+        // If there a selected Piece
+        //Debug.Log("StartFrag: " + startDrag + " End Drog: " + endDrag);
+    }
+
+    private void EndTurn()
+    {
+        // Для двойного прижка
+        int x = (int)endDrag.x;
+        int y = (int)endDrag.y;
+
+        // Promotions
+        // King
+        if(selectedPiece != null)
+        {
+            // Белая шашка приземлилась на конец борда
+            if(selectedPiece.isWhite && !selectedPiece.isKing && y == 7)
+            {
+                selectedPiece.isKing = true;
+                selectedPiece.transform.Rotate(Vector3.right * 180);
+            }
+            // Черная шашка приземлилась на конец борда
+            if(!selectedPiece.isWhite && !selectedPiece.isKing && y == 0)
+            {
+                selectedPiece.isKing = true;
+                selectedPiece.transform.Rotate(Vector3.right * 180);
+            }
+        }
+
+
+        selectedPiece = null;
+        startDrag = Vector2.zero;
+
+        // Сканировать на возможный ход (если убили)
+        if(ScanForPossibleMove(selectedPiece, x, y).Count != 0 && hasKilled)
+            return;
+
+        isWhiteTurn = !isWhiteTurn;
+        // Чтобы изменять ход команд
+        isWhite = !isWhite;
+        hasKilled = false;
+        
+        CheckVictory();
+    }
+
+    private void CheckVictory()
+    {
+        var ps = FindObjectsOfType<Piece>();
+        bool hasWhite = false, hasBlack = false;
+        for (int i = 0; i < ps.Length; i++)
+        {
+            if(ps[i].isWhite)
+            {
+                hasWhite = true;
+            }
+            else
+            {
+                hasBlack = true;
+            }
+        }
+
+        if(!hasWhite)
+        {
+            Victory(false);
+        }
+        if(!hasBlack)
+        {
+            Victory(true);
+        }
+
+    }
+
+    private void Victory(bool isWhite)
+    {
+        if(isWhite)
+            Debug.Log("White team has won");
+        else
+            Debug.Log("Black team has won");
+    }
+
+    // Сканирование для одной шашки
+    private List<Piece> ScanForPossibleMove(Piece p, int x, int y)
+    {
+        forcedPieces = new List<Piece>();
+
+        if(pieces[x, y].isForceToMove(pieces, x, y))
+        {
+            forcedPieces.Add(pieces[x, y]);
+        }
+
+        return forcedPieces;
+    }
+
+    private List<Piece> ScanForPossibleMove()
+    {
+        forcedPieces = new List<Piece>();
+
+        // Check all the pieces
+        // 8 вместо pieces.Lenght потому что удаляются 
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                if(pieces[i,j] != null && pieces[i, j].isWhite == isWhiteTurn)
+                {
+                    if(pieces[i,j].isForceToMove(pieces, i, j))
+                    {
+                        forcedPieces.Add(pieces[i, j]);
+                    }
+                }
+            }
+        }
+
+        return forcedPieces;
+    }
+
+    private void GenerateBoard()
+    {
+        // Generate White Team
+        for(int y = 0; y < 3; y++)
+        {
+            bool oddRow = (y % 2 == 0);
+            for(int x = 0; x < 8; x += 2)
+            {
+                //Generate our Piece
+                GeneratePiece((oddRow) ? x : x+1, y);
+            }
+        }
+
+         // Generate Black Team
+        for(int y = 7; y > 4; y--)
+        {
+            bool oddRow = (y % 2 == 0);
+            for(int x = 0; x < 8; x +=2)
+            {
+                //Generate our Piece
+                GeneratePiece((oddRow) ? x : x+1, y);
+            }
+        }
+    }
+
+    private void GeneratePiece(int x, int y)
+    {
+        bool isPieceWhite = (y > 4) ? false : true;
+        // Spawn of whitePieces or blackPiece
+        GameObject go = Instantiate((isPieceWhite)?whitePiecePrefab : blackPiecePrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieces[x, y] = p;
+        MovePiece(p, x, y);
+    }
+
+    private void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
+    }
+
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public bool isWhite;
+    public bool isKing;
+
+
+    public bool isForceToMove(Piece[,] board, int x, int y)
+    {
+        if(isWhite || isKing)
+        {
+            // Проверяем две клетке слева от нас и справа от нас
+            // Kill Top left 
+            if(x>=2 && y<=5)
+            {
+                Piece p = board[x - 1, y + 1];
+                // If there is a piece, and it is not the same color as ours
+                if(p != null && p.isWhite != isWhite)
+                {
+                    // Check it its possible to land after the jump (можем преземлиться)
+                    if(board[x-2, y+2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+
+            // Kill Top right 
+            if(x<=2 && y<=5)
+            {
+                Piece p = board[x + 1, y + 1];
+                // If there is a piece, and it is not the same color as ours
+                if(p != null && p.isWhite != isWhite)
+                {
+                    // Check it its possible to land after the jump (можем преземлиться)
+                    if(board[x+2, y+2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        // Для черной команды
+        
+        if(!isWhite || isKing)
+        {
+            // Kill Bot left 
+            if(x>=2 && y>=2)
+            {
+                Piece p = board[x - 1, y - 1];
+                // If there is a piece, and it is not the same color as ours
+                if(p != null && p.isWhite != isWhite)
+                {
+                    // Check it its possible to land after the jump (можем преземлиться)
+                    if(board[x-2, y-2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+
+            // Kill Bot right 
+            if(x<=2 && y>=2)
+            {
+                Piece p = board[x + 1, y - 1];
+                // If there is a piece, and it is not the same color as ours
+                if(p != null && p.isWhite != isWhite)
+                {
+                    // Check it its possible to land after the jump (можем преземлиться)
+                    if(board[x+2, y-2] == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    // Метод для правильного передвежения шашек
+    public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2)
+    {
+        // If you are moving on top of another piece
+        if(board[x2, y2] != null)
+        {
+            return false;
+        }
+        // Берем абсолютное значение
+        int deltaMove = Mathf.Abs(x1 - x2);
+        // Нам понадобится -1 когда мы находимся в черной команде
+        int deltaMoveY = y2 - y1;
+
+        // For white team
+        if(isWhite || isKing)
+        {
+            if(deltaMove == 1)
+            {
+                if(deltaMoveY == 1)
+                {
+                    return true;
+                }
+            }
+            // kill piece
+            else if(deltaMove == 2)
+            {
+                if(deltaMoveY == 2)
+                {
+                    // Для получения средней шашки между двумя
+                    Piece p = board[(x1+x2)/2, (y1+y2)/2];
+                    if(p != null && p.isWhite != isWhite)
+                        return true;
+                }
+            }
+        }
+
+        // For black team
+            if(!isWhite || isKing)
+        {
+            if(deltaMove == 1)
+            {
+                if(deltaMoveY == -1)
+                {
+                    return true;
+                }
+            }
+            // kill piece
+            else if(deltaMove == 2)
+            {
+                if(deltaMoveY == -2)
+                {
+                    // Для получения средней шашки между двумя
+                    Piece p = board[(x1+x2)/2, (y1+y2)/2];
+                    if(p != null && p.isWhite != isWhite)
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
